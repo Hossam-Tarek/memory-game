9453e3337c82c55b9d339ea991a765a2bcb16a24
mgado148 <mgado148@gmail.com>
2020-10-24 19:39:31 +0200

    Add new line add import

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
 import Grid from "./Grid.js";
 
-
-var card = document.querySelectorAll('.card');
-card.forEach(card => card.addEventListener('click', flipCard));
+var cards = document.querySelectorAll('.card');
+cards.forEach(card => card.addEventListener('click', flipCard));
 
 function flipCard(){
-    this.children[0].classList.toggle("front-toggle");
+    this.children[0].classList.toggle("flip");
     this.classList.toggle("disabled");
 }
--- js/card.js
+++ js/card.js
@@ -1,3 +1,4 @@
+import Grid from "./Grid.js";
 
 var cards = document.querySelectorAll('.card');
 cards.forEach(card => card.addEventListener('click', flipCard));
